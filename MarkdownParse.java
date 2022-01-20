95783bd6ab7c66b00c70642040cfd96bbf06ff0a
til026 <97641884+til026@users.noreply.github.com>
2022-01-19 19:18:55 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,16 +10,21 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        System.out.println(currentIndex);
+        // System.out.println(currentIndex);
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1){
+                break;
+            }
+            if ((nextOpenBracket == 0 || markdown.charAt(nextOpenBracket-1) != '!') && openParen == nextCloseBracket + 1){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
-        System.out.println(currentIndex);
+        // System.out.println(currentIndex);
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
