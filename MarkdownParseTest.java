9b8a8e6a16aa9fa8c9329dfad8ad6c088afff618
til026 <97641884+til026@users.noreply.github.com>
2022-02-02 18:45:39 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -67,4 +67,11 @@ public class MarkdownParseTest {
         List<String> expect = List.of("a link on the first line");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
+
+   @Test
+   public void testMyfile() throws IOException {
+        String contents= Files.readString(Path.of("./my-file1.md"));
+        List<String> expect = List.of("(this is [a] link)");
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+   }
 }
