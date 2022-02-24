f24f72fd278c68acfbadfc4b980246395525f8a8
til026 <97641884+til026@users.noreply.github.com>
2022-02-24 10:09:40 -0800

    revise

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -26,7 +26,7 @@ public class MarkdownParse {
                 }
             }
             if (counter > 1){currentIndex = closeParen + 1; continue;}
-            if ((nextOpenBracket == 0 || markdown.charAt(nextOpenBracket-1) != '!') && openParen == nextCloseBracket + 1){
+            if ((nextOpenBracket == 0 || markdown.charAt(nextOpenBracket-1) != '!') && markdown.charAt(openParen-1) == ']'){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             //some changes
