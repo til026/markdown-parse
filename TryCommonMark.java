5b0e643c0f24ca2eea7a58420e809b0689a56268
til026 <97641884+til026@users.noreply.github.com>
2022-03-09 19:01:32 -0800

    for Link

--- TryCommonMark.java
+++ TryCommonMark.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -29,5 +31,20 @@ class WordCountVisitor extends AbstractVisitor {
 
         // Descend into children (could be omitted in this case because Text nodes don't have children).
         visitChildren(text);
+    }
+}
+
+class linkCollecterVisitor extends AbstractVisitor {
+    ArrayList<String> linkCollecter = new ArrayList<String>();
+
+    @Override
+    public void visit(Link link) {
+        // This is called for all Text nodes. Override other visit methods for other node types.
+
+        // Count words (this is just an example, don't actually do it this way for various reasons).
+        linkCollecter.add(link.getDestination());
+
+        // Descend into children (could be omitted in this case because Text nodes don't have children).
+        visitChildren(link);
     }
 }
